5b296dd36c4330f04096f9784530b637bc7ccfac
Gabriele Tocchio <97604300+GabrieleTocchio@users.noreply.github.com>
2022-02-16 14:12:59 +0100

    Maratona
    App Maratona

---
+++ TocchioMaratonaApp/TocchioMaratonaApp/Maratona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TocchioMaratonaApp
+{
+    class Maratona
+    {
+
+        public string NomeAtleta { get; set; }
+        public string TitoloBrano { get; set; }
+        public string Minuti { get; set; }
+        public string Secondi { get; set; }
+        public string TitoloAlbum { get; set; }
+
+
+
+
+
+
+
+
+    }
+}
